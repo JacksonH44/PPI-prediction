52811fe1ff0664a8a3e9bd61a3c7126f7ee2b8c4
Jackson Howe <jhowe4@beluga3.int.ets1.calculquebec.ca>
2024-05-15 16:09:25 -0400

    add basic logging

--- bin/fasta_one_to_many.py
+++ bin/fasta_one_to_many.py
@@ -5,10 +5,11 @@ many smaller files for each transcript.
 
 
 import argparse
+import logging
 import os
 
 
-def fasta_one_to_many(file_path):
+def process_file(file_path):
     pass
 
 
@@ -19,7 +20,13 @@ def parse_command_line():
                         help='Input file name')
     parser.add_argument('-o', '--outfolder',
                         type=str, default=None,
-                        help='Output folder path')
+                        help='Output folder path (requires absolute path)')
+    parser.add_argument('-v', '--verbose',
+                        action='store_true',
+                        help='Change logging level from default level to noisiest level')
+    parser.add_argument('-l', '--logfile',
+                        type=str, default=None,
+                        help='Specify the name of the log file')
     args = parser.parse_args()
     return args
 
@@ -27,7 +34,18 @@ def parse_command_line():
 def main():
     """Run the command line program."""
     args = parse_command_line()
-    outfolder = os.getcwd()
+    outfolder = args.outfolder 
+    if args.outfolder is None:
+        outfolder = os.path.join(os.getcwd(), args.infile.rsplit('.', 1)[0])
+    logfile = args.logfile if args.logfile is not None else 'fasta_one_to_many.log'
+    logging_level = logging.DEBUG if args.verbose else logging.WARNING
+    logging.basicConfig(level=logging_level, filename=logfile)
+    logging.info(f'Saving files to {outfolder}')
+    try:
+        process_file(args.infile)
+    except FileNotFoundError:
+        msg = f'{args.infile} not processed: File does not exist'
+        logging.warning(msg)
 
 if __name__ == '__main__':
     main()
