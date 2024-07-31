eada5efbd92b36af412092ce6d5c6193622f1ab7
Jackson Howe <jacksonhowe3@gmail.com>
2024-07-31 12:12:54 -0400

    feat(frustration): calculate delta frustration given multimer and monomer (#51)

--- src/features/frustration.py
+++ src/features/frustration.py
@@ -6,6 +6,7 @@ energetic frustration in complexes.
 
 import logging
 import os
+import re
 import subprocess
 import sys
 
@@ -14,13 +15,38 @@ import pandas as pd
 sys.path.append(os.path.join(os.path.dirname(__file__), "../.."))
 from src.features.file_utils import find_pdb_files
 from src.features.interaction_site import (
+    apply_residue_mask,
     get_contact_map,
     find_interaction_site,
     find_length_split
 )
 
 
-def calculate_frstindex(pdb_file: str, chain: str) -> float:
+def calculate_avg_frst_index_delta(multimer_residues: list[float], monomer_residues: list[float]) -> float:
+    """
+    Calculate the change in frustration index between a multimer
+    and monomer complex.
+
+    Parameters
+    ----------
+    multimer_residues : list[float]
+        The list of frustration indices for the multimer residues
+    monomer_residues : list[float]
+        The list of frustration indices for the monomer residues
+
+    Returns
+    -------
+    float
+        The difference in average frustration index between the multimer
+        and monomer
+    """
+    assert len(monomer_residues) == len(
+        multimer_residues
+    ), "The lengths of monomer and multimer residues should be the same"
+    return round((sum(multimer_residues) - sum(monomer_residues)) / len(monomer_residues), 4)
+
+
+def get_frstindex(pdb_file: str, chain: str) -> list[float]:
     """
     Calculate the average frstIndex across all residues of a PDB file.
 
@@ -35,9 +61,8 @@ def calculate_frstindex(pdb_file: str, chain: str) -> float:
 
     Returns
     -------
-    avg_frst_index : float
-        The average frustration index across all residues in the chain, rounded to 4
-        significant figures
+    list[float]
+        A list of residue-level frustration index values
     """
     subprocess.run([
         "Rscript",
@@ -59,67 +84,146 @@ def calculate_frstindex(pdb_file: str, chain: str) -> float:
         sep=' ',
         usecols=['FrstIndex']
     )
-    avg_frst_index = round(frst_df['FrstIndex'].sum() / frst_df.shape[0], 4)
-    return avg_frst_index
+    return frst_df['FrstIndex'].to_list()
 
 
-def frustration_stats(complex: str, complex_dir: str, monomer_dir: str) -> list[str]:
+def calculate_deltas(
+        symbol: str,
+        multimer_file: str, 
+        monomer_file: str, 
+        partner_num: int,
+        mask_map: dict[str, list[bool]]
+) -> tuple[float, float]:
     """
-    Return the avg, max, and min change in FrstIndex for interaction and
-    non-interaction site on both proteins in a complex.
+    Calculate the interaction and non-interaction site deltas for a gene in a complex.
+
+    Parameters
+    ----------
+    symbol : str
+        Gene symbol
+    multimer_file : str
+        The full path to the multimer file
+    monomer_file : str
+        The full path to the monomer file
+    partner_num : int
+        The number partner the gene is (either 0 or 1)
+    mask_map : dict[str, list[bool]]
+        A map of gene symbols to its interaction site mask
+
+    Returns
+    -------
+    tuple[float, float]
+        The delta values for the interaction and non-interaction site
+    """
+    multimer_frst_index = get_frstindex(multimer_file, 'A' if partner_num == 0 else 'B')
+    monomer_frst_index = get_frstindex(monomer_file, 'A')
+    # divide multimer and monomer SAs into interaction and non-interaction
+    multimer_interaction, multimer_non_interaction = apply_residue_mask(
+        multimer_frst_index, mask_map[symbol]
+    )
+    monomer_interaction, monomer_non_interaction = apply_residue_mask(
+        monomer_frst_index, mask_map[symbol]
+    )
+    interaction_delta = calculate_avg_frst_index_delta(
+        monomer_interaction, multimer_interaction
+    )
+    non_interaction_delta = calculate_avg_frst_index_delta(
+        monomer_non_interaction, multimer_non_interaction
+    )
+    return (interaction_delta, non_interaction_delta)
+
+
+def frustration_stats(
+        complex: str, complex_dir: str, monomer_dir: str, num_models: int = 1
+) -> list[float]:
+    """
+    Return the avg, max, and min change in surface area for interaction
+    and non-interaction site on a complex.
 
     Parameters
     ----------
     complex : str
-        The name of the complex to get the frustration stats for
+        The name of the complex to get stats for
     complex_dir : str
         The path to the bottom-most directory that contains the complex
     monomer_dir : str
-        The path to the top-level monomer directory that contains all
-        monomer ColabFold outputs
+        The path to the top-level monomer directory that contains all monomer
+        Colabfold outputs
+    num_models : int
+        The number of AlphaFold models you'd like to take into account (defaults to 1,
+        use 5 if you'd like to include all models)
 
     Returns
     -------
     list[str]
-        A 12 element list consisting of avg, max, and min change in FrstIndex
-        for the interacting and non-interacting sites of both proteins in the
-        complex
+        A 12 element list in the following order (i: interaction site, ni: non-interaction site,
+        sa: surface area, 1: cancer driver gene, 2: interacting partner):
+        [min_i_sa_1, min_i_sa_2, max_i_sa_1, max_i_sa_2, avg_i_sa_1, avg_i_sa_2,
+        min_ni_sa_1, min_ni_sa_2, max_ni_sa_1, max_ni_sa_1, avg_ni_sa_1, avg_ni_sa_2]
 
     Usage
     -----
-    e.g., surface_area_stats('CDKN2A_CYCS', 'tests/test_data/colabfold/0', 'tests/test_data/colabfold/monomer')
+    frustration_stats(
+        'CDKN2A_CYCS',
+        'tests/test_data/colabfold/0',
+        'tests/test_data/colabfold/monomer',
+        5
+    )
     """
-    multimer_pdb_files = find_pdb_files(complex_dir)
-    multimer_pdb = [file for file in multimer_pdb_files if complex in file]
-    # Ensure that exactly 1 PDB file is found for the complex
-    assert (
-        len(multimer_pdb) == 1
-    ), f"Unable to find 1 file for {complex} in {complex_dir}"
-    multimer_pdb_file = multimer_pdb[0]
-    logging.debug(f"Found multimer PDB file: {multimer_pdb_file}")
-    # Find all potential monomer PDB files that might be needed
+    all_pdb_files = find_pdb_files(complex_dir, num_models)
+    multimer_pdb = [file for file in all_pdb_files if complex in file]
     monomers = os.listdir(monomer_dir)
     monomer_pdb_files = [
-        pdb_file
+        os.path.join(m, pdb_file)
         for m in monomers
-        for pdb_file in find_pdb_files(os.path.join(monomer_dir, m))
+        for pdb_file in find_pdb_files(os.path.join(monomer_dir, m), num_models)
     ]
+    split = find_length_split(complex, complex_dir)
+    if split is None:
+        return [float("nan")] * 12
+    
+    min_i_f = [float("inf"), float("inf")]
+    max_i_f = [float("-inf"), float("-inf")]
+    avg_i_f = [0.0, 0.0]
+    min_ni_f = [float("inf"), float("inf")]
+    max_ni_f = [float("-inf"), float("-inf")]
+    avg_ni_f = [0.0, 0.0]
 
-    symbol = multimer_pdb_file.split("/")[-1].split(".")[0]
-    split = find_length_split(symbol, complex_dir)
-    if split:
+
+
+    for rank in range(1, num_models + 1):
+        multimer_pattern = rf"^{complex}.msa_unrelaxed_rank_00{rank}_alphafold2_multimer_v3_model_\d+_seed_\d+\.pdb$"
+        multimer_pdb_file = [
+            file for file in multimer_pdb if re.match(multimer_pattern, file)
+        ][0]
+        # generate contact map for this model
         file_path = os.path.join(complex_dir, multimer_pdb_file)
-        seq_length_1, seq_length_2 = split[0], split[1]
-        logging.debug(f"Generating contact map for {symbol}...")
         cmap_df = get_contact_map(file_path)
-        logging.debug(f"Finding interaction site for {symbol}...")
-        mask_map = find_interaction_site(symbol, cmap_df, seq_length_1, seq_length_2)
-        logging.debug(f"Calculating multimer surface area structure for {symbol}...")
+        # find the interaction site for this complex based on the contact map
+        mask_map = find_interaction_site(complex, cmap_df, split[0], split[1])
+
+        # analyze monomers
+        for i in [0, 1]:
+            symbol = complex.split("_")[i]
+            # calculate multimer frustration index
+            multimer_frst_index = get_frstindex(
+                os.path.join(complex_dir, multimer_pdb_file),
+                'A' if i == 0 else 'B'
+            )
+            monomer_pattern = rf".*_{symbol}.msa_unrelaxed_rank_001_alphafold2_ptm_model_\d+_seed_\d+\.pdb"
+            monomer_file = [
+                pdb_file
+                for pdb_file in monomer_pdb_files
+                if re.match(monomer_pattern, pdb_file.split("/")[-1])
+            ][0]
+            # calculate frustration index for the monomer
+            monomer_frst_index = get_frstindex(os.path.join(monomer_dir, monomer_file), 'A')
 
 
 if __name__ == '__main__':
-    avg_frst_index = calculate_frstindex(
-        '/cluster/projects/kumargroup/jackson/colabfold/0/CDKN2A_CYCS.msa_unrelaxed_rank_001_alphafold2_multimer_v3_model_5_seed_000.pdb',
-        'A'
+    frustration_stats(
+        'CDKN2A_CYCS',
+        'tests/test_data/colabfold/0',
+        'tests/test_data/colabfold/monomer',
+        5
     )
-    print(avg_frst_index)
