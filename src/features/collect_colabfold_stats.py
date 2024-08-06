18c0bc6176de3cb6b389c850170dbb66162f989b
Jackson Howe <jacksonhowe3@gmail.com>
2024-08-06 11:42:34 -0400

    fix(features): refactor collect_colabfold_stats to pull data from score files instead of logs (#51), (#54)

--- src/features/collect_colabfold_stats.py
+++ src/features/collect_colabfold_stats.py
@@ -6,111 +6,88 @@ an error in the run.
 
 import argparse
 import csv
+import json
 import logging
 import os
 import sys
-
-import pandas as pd
+import time
+import re
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../.."))
 from src.features.file_utils import find_all_complexes
+from src.features.find_stats import find_stats
 
 
-def _extract_metrics_from_line(
-    line: str,
-) -> tuple[float, float, float]:  # pragma: no cover
+def get_colabfold_metrics(symbol: str, data_dir: str) -> list[str]:
     """
-    Take a line of the form:
-
-    rank_001_alphafold2_multimer_v3_model_4_seed_000 pLDDT=70.2 pTM=0.663 ipTM=0.135
-
-    and return pLDDT, pTM, and ipTM
-    """
-    plddt = line.split(" ")[1].split("=")[1]
-    ptm = line.split(" ")[2].split("=")[1]
-    iptm = line.split(" ")[3].split("=")[1]
-    return float(plddt), float(ptm), float(iptm)
-
-
-def get_colabfold_metrics(symbol: str, lines: list[str]) -> list[str]:
-    """
-    Return the best model, pLDDT score, and ipTM score from the ColabFold log
-    output file.
+    Return the max predicted aligned error (PAE), the pLDDT score, ipTM score,
+    and pTM score for a given complex.
 
     Parameters
     ----------
     symbol : str
         The complex symbol
-    lines : list[str]
-        The processed lines of the logfile ready for metric extraction
+    data_dir : str
+        The path to the directory containing the scores for the complex
 
     Returns
     -------
-    list[str]
-        A 13 element list representing the best model (first element in list)
-        and 12 features: best model, average, max, and min for each of pLDDT,
-        pTM, and ipTM.
+    str_features : list[str]
+        A 10 element list representing 10 features: max PAE, average, max, and
+        min for each of pLDDT, pTM, and ipTM.
     """
-    # Find the start and the end of the log file for this symbol
-    # Line should say something like 'Query 1/4 CDKN2C_CD24.msa (length 248)'
-    query_start = [line for line in lines if symbol in line][0]
-    query_start_index = lines.index(query_start)
-    query_end = [
-        lines[i]
-        for i in range(query_start_index + 1, len(lines))
-        if "Query" in lines[i] or "Done" in lines[i]
-    ][0]
-    query_end_index = lines.index(query_end)
-    lines = lines[query_start_index:query_end_index]
-    stats_lines = lines[-5:]
-    best_model = stats_lines[0].split(" ")[0].split("_")[6]
-    best_model_plddt, best_model_ptm, best_model_iptm = _extract_metrics_from_line(
-        stats_lines[0]
-    )
-    plddt_total = 0.0
-    ptm_total = 0.0
-    iptm_total = 0.0
-    max_plddt = 0.0
-    min_plddt = 100.0
-    max_ptm = 0.0
-    min_ptm = 1.0
-    max_iptm = 0.0
-    min_iptm = 1.0
-    for line in stats_lines:
-        plddt, ptm, iptm = _extract_metrics_from_line(line)
-        plddt_total += plddt
-        max_plddt = max(max_plddt, plddt)
-        min_plddt = min(min_plddt, plddt)
-        ptm_total += ptm
-        max_ptm = max(max_ptm, ptm)
-        min_ptm = min(min_ptm, ptm)
-        iptm_total += iptm
-        max_iptm = max(max_iptm, iptm)
-        min_iptm = min(min_iptm, iptm)
-    mean_plddt = round(plddt_total / 5, 4)
-    mean_ptm = round(ptm_total / 5, 4)
-    mean_iptm = round(iptm_total / 5, 4)
+    ptm_regex = rf"^{symbol}.msa_scores_rank_00\d+_alphafold2_multimer_v3_model_\d+_seed_000.json"
+    ptm_scores = [file for file in os.listdir(data_dir) if re.search(ptm_regex, file)]
+    max_ptm, max_iptm, max_plddt = float("-inf"), float("-inf"), float("-inf")
+    min_ptm, min_iptm, min_plddt = float("inf"), float("inf"), float("inf")
+    avg_ptm, avg_iptm, avg_plddt = 0.0, 0.0, 0.0
+    for ptm_score_file in ptm_scores:
+        with open(os.path.join(data_dir, ptm_score_file), "r") as ptm_json:
+            score = json.load(ptm_json)
+            ptm = score["ptm"]
+            iptm = score["iptm"]
+            raw_plddt = score["plddt"]
+            plddt = sum(raw_plddt) / len(raw_plddt)
+            max_ptm = max(max_ptm, ptm)
+            max_iptm = max(max_iptm, iptm)
+            max_plddt = max(max_plddt, plddt)
+            min_ptm = min(min_ptm, ptm)
+            min_iptm = min(min_iptm, iptm)
+            min_plddt = min(min_plddt, plddt)
+            avg_ptm += ptm
+            avg_iptm += iptm
+            avg_plddt += plddt
+    avg_ptm = round(avg_ptm / len(ptm_scores) if avg_ptm != 0.0 else 0.0, 2)
+    avg_iptm = round(avg_iptm / len(ptm_scores) if avg_iptm != 0.0 else 0.0, 2)
+    avg_plddt = round(avg_plddt / len(ptm_scores) if avg_plddt != 0.0 else 0.0, 2)
+    max_plddt = round(max_plddt, 4)
+    min_plddt = round(min_plddt, 4)
 
-    features = [
-        best_model,
-        best_model_plddt,
-        mean_plddt,
+    # Get predicted aligned error score
+    pae_regex = rf"^{symbol}.msa_predicted_aligned_error_v1.json"
+    pae_file = [file for file in os.listdir(data_dir) if re.search(pae_regex, file)]
+    assert len(pae_file) == 1, f"PAE file for {symbol} is missing"
+    with open(os.path.join(data_dir, pae_file[0]), "r") as pae_json:
+        score = json.load(pae_json)
+        pae = score["max_predicted_aligned_error"]
+
+    float_features = [
+        pae,
+        avg_plddt,
         max_plddt,
         min_plddt,
-        best_model_ptm,
-        mean_ptm,
+        avg_ptm,
         max_ptm,
         min_ptm,
-        best_model_iptm,
-        mean_iptm,
+        avg_iptm,
         max_iptm,
         min_iptm,
     ]
-    features_str = [str(feat) for feat in features]
-    return features_str
+    str_features = [str(feat) for feat in float_features]
+    return str_features
 
 
-def collect_stats(data_dir: str, stats_file: str):
+def collect_stats(data_dir: str, monomer_dir: str, stats_file: str):
     """
     Scan the data directory and collect stats
     for each new complex created by ColabFold.
@@ -119,49 +96,60 @@ def collect_stats(data_dir: str, stats_file: str):
     ----------
     data_dir : str
         Path to the data directory
+    monomer_dir : str
+        Path to the monomer file directory
     stats_file : str
         Path to the stats file. Assumes stats_file is a CSV
         file with headers symbol,plddt,iptm,best_model
     """
-    if os.path.exists(stats_file):
-        symbols_needed = find_all_complexes(data_dir)
-        symbols_computed = pd.read_csv(stats_file, usecols=["symbol"])[
-            "symbol"
-        ].to_list()
-        symbols = list(set(symbols_needed) - set(symbols_computed))
-    else:
-        symbols = find_all_complexes(data_dir)
-        with open(stats_file, "w") as stats:
-            writer = csv.writer(stats)
-            header = [
-                "symbol",
-                "best_model",
-                "best_model_plddt",
-                "mean_plddt",
-                "max_plddt",
-                "min_plddt",
-                "best_model_ptm",
-                "mean_ptm",
-                "max_ptm",
-                "min_ptm",
-                "best_model_iptm",
-                "mean_iptm",
-                "max_iptm",
-                "min_iptm",
-            ]
-            writer.writerow(header)
-    logfile = os.path.join(data_dir, "log.txt")
-    with open(logfile, "r") as log:
-        lines = log.readlines()
-        lines = [line.split(" ", maxsplit=2)[2] for line in lines]
-        lines = [line.rstrip("\n") for line in lines]
+    symbols = find_all_complexes(data_dir)
+    with open(stats_file, "w") as stats:
+        writer = csv.writer(stats)
+        header = [
+            "symbol",
+            "max_pae",
+            "mean_plddt",
+            "max_plddt",
+            "min_plddt",
+            "mean_ptm",
+            "max_ptm",
+            "min_ptm",
+            "mean_iptm",
+            "max_iptm",
+            "min_iptm",
+            "min_sa_i_1",
+            "min_sa_i_2",
+            "max_sa_i_1",
+            "max_sa_i_2",
+            "avg_sa_i_1",
+            "avg_sa_i_2",
+            "min_sa_ni_1",
+            "min_sa_ni_2",
+            "max_sa_ni_1",
+            "max_sa_ni_2",
+            "avg_sa_ni_1",
+            "avg_sa_ni_2",
+            "min_fi_i_1",
+            "min_fi_i_2",
+            "max_fi_i_1",
+            "max_fi_i_2",
+            "avg_fi_i_1",
+            "avg_fi_i_2",
+            "min_fi_ni_1",
+            "min_fi_ni_2",
+            "max_fi_ni_1",
+            "max_fi_ni_2",
+            "avg_fi_ni_1",
+            "avg_fi_ni_2",
+        ]
+        writer.writerow(header)
         for symbol in symbols:
-            features = []
-            features.append(symbol)
-            features += get_colabfold_metrics(symbol, lines)
-            with open(stats_file, "a") as stats:
-                writer = csv.writer(stats)
-                writer.writerow(features)
+            features = [symbol]
+            colabfold_metrics = get_colabfold_metrics(symbol, data_dir)
+            features += colabfold_metrics
+            features += find_stats(symbol, data_dir, monomer_dir, "surface_area", 5)
+            features += find_stats(symbol, data_dir, monomer_dir, "frustration", 5)
+            writer.writerow(features)
 
 
 def parse_command_line():  # pragma : no cover
@@ -171,8 +159,15 @@ def parse_command_line():  # pragma : no cover
         "-d",
         "--data_directory",
         type=str,
-        default="data/processed/colabfold/0",
+        default="tests/test_data/colabfold/0",
         help="The path to the directory holding all finished ColabFold outputs",
+    )
+    parser.add_argument(
+        "-m",
+        "--monomer_directory",
+        type=str,
+        default="tests/test_data/colabfold/monomer",
+        help="The path to the directory holding all monomer ColabFold outputs",
     )
     parser.add_argument(
         "-s",
@@ -212,7 +207,10 @@ def main():  # pragma: no cover
             file.write("")  # Write an empty string to create the file
     logging_level = logging.DEBUG if args.verbose else logging.INFO
     logging.basicConfig(level=logging_level, filename=logfile, filemode="w")
-    collect_stats(args.data_directory, args.stats_file)
+    start = time.perf_counter()  # Time the function call for debugging
+    collect_stats(args.data_directory, args.monomer_directory, args.stats_file)
+    finish = time.perf_counter()
+    logging.info(f"Finished curation in {round(finish - start, 2)} second(s)")
 
 
 if __name__ == "__main__":  # pragma: no cover
