fafaa3b649f1de564d48f89cb05a7073ceeed544
Jackson Howe <jacksonhowe3@gmail.com>
2024-08-06 14:31:18 -0400

    fix: fix bug in find_pdb_files returning too many files (#54)

--- src/features/file_utils.py
+++ src/features/file_utils.py
@@ -59,31 +59,20 @@ def find_pdb_files(batch_path: str, num_models: int = 1) -> list[str]:
     pdb_list = []
     complex_symbols = find_all_complexes(batch_path)
     for symbol in complex_symbols:
-        base_pdb_pattern = (
-            rf"^{symbol}.*\.pdb"
-        )
+        base_pdb_pattern = rf".*_{symbol}.pdb"
         base_pdbs = [
-            file
-            for file in os.listdir(batch_path)
-            if re.match(base_pdb_pattern, file)
+            file for file in os.listdir(batch_path) if re.match(base_pdb_pattern, file)
         ]
         if len(base_pdbs) != 0:
             pdb_list.append(base_pdbs[0])
         for model_no in range(1, num_models + 1):
-            multimer_pattern = (
-                rf"^{symbol}.msa_unrelaxed_rank_00{model_no}_alphafold2_multimer_v3_model_\d+_seed_\d+\.pdb$"
-            )
-            monomer_pattern = (
-                rf"^{symbol}.msa_unrelaxed_rank_00{model_no}_alphafold2_ptm_model_\d+_seed_\d+\.pdb"
-            )
+            multimer_pattern = rf"^{symbol}.msa_unrelaxed_rank_00{model_no}_alphafold2_multimer_v3_model_\d+_seed_\d+\.pdb$"
+            monomer_pattern = rf"^{symbol}.msa_unrelaxed_rank_00{model_no}_alphafold2_ptm_model_\d+_seed_\d+\.pdb"
             files_found = [
-                    file
-                    for file in os.listdir(batch_path)
-                    if re.match(multimer_pattern, file)
-                    or re.match(monomer_pattern, file)
+                file
+                for file in os.listdir(batch_path)
+                if re.match(multimer_pattern, file) or re.match(monomer_pattern, file)
             ]
             if len(files_found) != 0:
-                pdb_list.append(
-                    files_found[0]
-                )
+                pdb_list.append(files_found[0])
     return pdb_list
