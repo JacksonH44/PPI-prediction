9bdf00a0466602e4cf922c5e1c630ceea35303cc
Jackson Howe <jacksonhowe3@gmail.com>
2024-08-14 11:10:01 -0400

    style(visualization): fix FrustrationPlotter linter error for _ylabel (#56)

--- src/visualization/FrustrationPlotter.py
+++ src/visualization/FrustrationPlotter.py
@@ -28,18 +28,20 @@ class FrustrationPlotter(Plotter):
         Create features for plotting frustration index
         """
         self._title = "Boxplot of Change in Energetic Frustration"
-        self._ylabel = "$\Delta$ Frustration Index"
+        self._ylabel = "$\Delta$ Frustration Index"  # noqa: W605
 
         # Create data object
-        stats_df = pd.read_csv(self._stats_file_path, usecols=["symbol", 'avg_fi_i_1', 'avg_fi_i_2'])
+        stats_df = pd.read_csv(
+            self._stats_file_path, usecols=["symbol", "avg_fi_i_1", "avg_fi_i_2"]
+        )
         neg_fi_i_1 = []
         neg_fi_i_2 = []
         pos_fi_i_1 = []
         pos_fi_i_2 = []
         for _, row in stats_df.iterrows():
-            sym = row['symbol']
-            avg_fi_i_1 = row['avg_fi_i_1']
-            avg_fi_i_2 = row['avg_fi_i_2']
+            sym = row["symbol"]
+            avg_fi_i_1 = row["avg_fi_i_1"]
+            avg_fi_i_2 = row["avg_fi_i_2"]
             if sym in self._negative_ppis:
                 neg_fi_i_1.append(avg_fi_i_1)
                 neg_fi_i_2.append(avg_fi_i_2)
@@ -48,11 +50,22 @@ class FrustrationPlotter(Plotter):
                 pos_fi_i_2.append(avg_fi_i_2)
         data = {
             self._ylabel: pos_fi_i_1 + neg_fi_i_1 + pos_fi_i_2 + neg_fi_i_2,
-            'Class': ['Cancer Driver (+)'] * len(pos_fi_i_1) + ['Cancer Driver (-)'] * len(neg_fi_i_1) + ['Partner (+)'] * len(pos_fi_i_2) + ['Partner (-)'] * len(neg_fi_i_2)
+            "Class": ["Cancer Driver (+)"] * len(pos_fi_i_1)
+            + ["Cancer Driver (-)"] * len(neg_fi_i_1)
+            + ["Partner (+)"] * len(pos_fi_i_2)
+            + ["Partner (-)"] * len(neg_fi_i_2),
         }
         self._data = pd.DataFrame(data)
-        self._pairs = [('Cancer Driver (+)', 'Cancer Driver (-)'), ('Partner (+)', 'Partner (-)')]
-        self._colour_palette = {'Cancer Driver (+)': '#36F1CD', 'Cancer Driver (-)': '#D770C7', 'Partner (+)': '#36F1CD', 'Partner (-)': '#D770C7'}
+        self._pairs = [
+            ("Cancer Driver (+)", "Cancer Driver (-)"),
+            ("Partner (+)", "Partner (-)"),
+        ]
+        self._colour_palette = {
+            "Cancer Driver (+)": "#36F1CD",
+            "Cancer Driver (-)": "#D770C7",
+            "Partner (+)": "#36F1CD",
+            "Partner (-)": "#D770C7",
+        }
 
 
 if __name__ == "__main__":
